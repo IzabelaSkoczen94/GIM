be5f195e5a5ae2d38a387bd548e3a31591633dd2
IzabelaSkoczen94 <38669972+IzabelaSkoczen94@users.noreply.github.com>
2018-04-24 00:04:59 +0200

    Initial commit

---
+++ lab3/lab3/Assets/Scripts/Pyramid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyramid : MonoBehaviour {
+
+	public int height = 5;
+    public float offsetX = 1;
+	public float offsetY = 2.05f;
+    public GameObject obj;
+
+
+    // Use this for initialization
+	void Start () {
+		int baseSize = height * 2 - 1;
+
+
+
+		for (int k = 0; k < height; k++)
+		{
+			for (int i = k; i < baseSize - k; i++)
+        	{
+				for (int j = k; j < baseSize - k; j++)
+            	{
+                	GameObject temp_obj = Instantiate(obj, transform, false);
+					temp_obj.transform.Translate(new Vector3(i * offsetX, k * offsetY, j * offsetX));
+            	}
+        	}
+		}
+	}
+}
